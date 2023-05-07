b78e263615b126976cdd074a558f00bf81511b08
Adeonir Kohl <adeonir@gmail.com>
2023-05-07 10:08:18 -0300

    add crypto uuid to todo object

--- core/index.ts
+++ core/index.ts
@@ -1,15 +1,18 @@
 import fs from "fs";
+import crypto from "crypto";
 
 const DB_PATH = "./data/db.json";
 
 type Todo = {
+  id: string
   date: string;
   content: string;
   done: boolean;
 }
 
-function create(content: string): string {
+function create(content: string): Todo {
   const todo: Todo = {
+    id: crypto.randomUUID(),
     date: new Date().toISOString(),
     content,
     done: false
@@ -21,7 +24,7 @@ function create(content: string): string {
   ]
 
   fs.writeFileSync(DB_PATH, JSON.stringify({todos}, null, 2));
-  return content;
+  return todo;
 }
 
 function read(): Todo[] {
@@ -32,6 +35,24 @@ function read(): Todo[] {
   return todos;
 }
 
+function update(id: string, partial: Partial<Todo>): Todo {
+  let updated
+
+  const todos = read();
+  todos.forEach(current => {
+    if (current.id === id) {
+      updated = Object.assign(current, partial);
+    }
+  })
+
+  if (!updated) {
+    throw new Error("Todo not found");
+  }
+
+  fs.writeFileSync(DB_PATH, JSON.stringify({todos}, null, 2));
+  return updated;
+}
+
 function cleanDb(): void {
   fs.writeFileSync(DB_PATH, '');
 }
@@ -40,4 +61,6 @@ function cleanDb(): void {
 cleanDb()
 console.log(create("Hello World"));
 console.log(create("Hello World Again"));
+const todo = create("Hello World Once More");
+update(todo.id, { done: true });
 console.log(read());
