c15d0132ef210ddcf1ce31925916cc73986ac894
Adeonir Kohl <adeonir@gmail.com>
2023-05-23 14:30:32 -0300

    fix with linter

--- core/index.ts
+++ core/index.ts
@@ -1,15 +1,16 @@
-import fs from "fs";
-import crypto from "crypto";
+/* eslint-disable no-console */
+import fs from 'fs'
+import crypto from 'crypto'
 
-const DB_PATH = "./data/db.json";
+const DB_PATH = './data/db.json'
 
-type UUID = string;
+type UUID = string
 
 type Todo = {
-  id: UUID;
-  date: string;
-  content: string;
-  done: boolean;
+  id: UUID
+  date: string
+  content: string
+  done: boolean
 }
 
 function create(content: string): Todo {
@@ -17,56 +18,52 @@ function create(content: string): Todo {
     id: crypto.randomUUID(),
     date: new Date().toISOString(),
     content,
-    done: false
+    done: false,
   }
 
-  const todos: Todo[] = [
-    ...read(),
-    todo
-  ]
+  const todos: Todo[] = [...read(), todo]
 
-  fs.writeFileSync(DB_PATH, JSON.stringify({todos}, null, 2));
-  return todo;
+  fs.writeFileSync(DB_PATH, JSON.stringify({ todos }, null, 2))
+  return todo
 }
 
 function read(): Todo[] {
-  const string = fs.readFileSync(DB_PATH).toString();
-  const todos = JSON.parse(string || "{}").todos as Todo[];
+  const string = fs.readFileSync(DB_PATH).toString()
+  const todos = JSON.parse(string || '{}').todos as Todo[]
 
-  if (!todos) return [];
-  return todos;
+  if (!todos) return []
+  return todos
 }
 
 function updateById(id: UUID, partial: Partial<Todo>): Todo {
-  const todos = read();
-  const toUpdate = todos.find(todo => todo.id === id);
+  const todos = read()
+  const toUpdate = todos.find((todo) => todo.id === id)
 
   if (!toUpdate) {
-    throw new Error("Todo not found");
+    throw new Error('Todo not found')
   }
 
-  const updated = { ...toUpdate, ...partial };
-  const updatedTodos = todos.map(todo => todo.id === id ? updated : todo);
+  const updated = { ...toUpdate, ...partial }
+  const updatedTodos = todos.map((todo) => (todo.id === id ? updated : todo))
 
-  fs.writeFileSync(DB_PATH, JSON.stringify({todos: updatedTodos}, null, 2));
-  return updated;
+  fs.writeFileSync(DB_PATH, JSON.stringify({ todos: updatedTodos }, null, 2))
+  return updated
 }
 
 function deleteById(id: UUID) {
-  const todos = read();
-  const filtered = todos.filter(todo => todo.id !== id);
-  fs.writeFileSync(DB_PATH, JSON.stringify({todos: filtered}, null, 2));
+  const todos = read()
+  const filtered = todos.filter((todo) => todo.id !== id)
+  fs.writeFileSync(DB_PATH, JSON.stringify({ todos: filtered }, null, 2))
 }
 
 function cleanDb(): void {
-  fs.writeFileSync(DB_PATH, '');
+  fs.writeFileSync(DB_PATH, '')
 }
 
 // Simulation
 cleanDb()
-const first = create("Hello World");
-const second = create("Hello World Again");
-const third = create("Hello World Once More");
-updateById(third.id, { done: true });
-deleteById(first.id);
-console.log(read());
+const first = create('Hello World')
+const second = create('Hello World Again')
+updateById(first.id, { done: true })
+deleteById(second.id)
+console.log(read())
