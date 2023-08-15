c9472717dbdbf8af1626acc870c57402b7f5f899
Adeonir Kohl <adeonir@gmail.com>
2023-08-14 21:39:25 -0300

    update next api todos handler

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { todosController } from '~/controllers/server/todos'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
     todosController.get(req, res)
     return
