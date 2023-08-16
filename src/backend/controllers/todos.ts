d7051741e3cf49add744f6b94390e3761a91a352
Adeonir Kohl <adeonir@gmail.com>
2023-08-16 10:17:36 -0300

    update backend controller to use new repository

--- src/backend/controllers/todos.ts
+++ src/backend/controllers/todos.ts
@@ -1,10 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { todosRepository } from '~/backend/repository/todos'
 
-import { read } from '~/crud'
+const get = (req: NextApiRequest, res: NextApiResponse) => {
+  const query = req.query
+  const page = Number(query.page)
+  const limit = Number(query.limit)
 
-const get = (_req: NextApiRequest, res: NextApiResponse) => {
-  const todos = read()
-  res.status(200).json({ todos })
+  if (query.page && isNaN(page)) {
+    return res.status(400).json({ error: { message: 'Page is not a number' } })
+  }
+
+  if (query.limit && isNaN(limit)) {
+    return res.status(400).json({ error: { message: 'Limit is not a number' } })
+  }
+
+  const response = todosRepository.get({ page, limit })
+
+  res.status(200).json({
+    total: response.total,
+    pages: response.pages,
+    todos: response.todos,
+  })
 }
 
 export const todosController = {
