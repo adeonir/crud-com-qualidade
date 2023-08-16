f76685d3f109cb72fc8b89b64164aebfbea140bf
Adeonir Kohl <adeonir@gmail.com>
2023-08-16 10:17:07 -0300

    add backend repository to deal with pagination

---
+++ src/backend/repository/todos.ts
@@ -0,0 +1,37 @@
+import { read } from '~/crud'
+
+type GetParams = {
+  page?: number
+  limit?: number
+}
+
+type GetResponse = {
+  todos: Todo[]
+  total: number
+  pages: number
+}
+
+type Todo = {
+  id: string
+  date: string
+  content: string
+  done: boolean
+}
+
+const get = ({ page, limit }: GetParams = {}): GetResponse => {
+  const todos = read()
+  const currentPage = page ?? 1
+  const currentLimit = limit ?? 10
+
+  const start = (currentPage - 1) * currentLimit
+  const end = currentPage * currentLimit
+
+  return {
+    todos: todos.slice(start, end),
+    total: todos.length,
+    pages: Math.ceil(todos.length / currentLimit),
+  }
+}
+export const todosRepository = {
+  get,
+}
