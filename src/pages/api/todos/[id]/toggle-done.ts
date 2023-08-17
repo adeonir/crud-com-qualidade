9b1915998dcd8a1d755d1693e22228b468c2d245
Adeonir Kohl <adeonir@gmail.com>
2023-08-17 09:23:50 -0300

    add toggle done api route

--- src/pages/api/todos/[id]/toggle-done.ts
+++ src/pages/api/todos/[id]/toggle-done.ts
@@ -1,14 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
 import { todosController } from '~/backend/controllers/todos'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    return todosController.get(req, res)
-  }
-
-  if (req.method === 'POST') {
-    return todosController.post(req, res)
+  if (req.method === 'PUT') {
+    return todosController.toggleDone(req, res)
   }
 
   res.status(405).json({ error: { message: 'Method not allowed' } })
