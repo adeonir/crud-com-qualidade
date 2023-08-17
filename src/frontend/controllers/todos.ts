2f5dcb5af9445dc51278cc8a5d14738a61ddeea4
Adeonir Kohl <adeonir@gmail.com>
2023-08-17 07:42:24 -0300

    update frontend controller to create new todo

--- src/frontend/controllers/todos.ts
+++ src/frontend/controllers/todos.ts
@@ -4,6 +4,12 @@ type GetParams = {
   page: number
 }
 
+type PostParams<T> = {
+  content: string
+  onSuccess?: (todo: T) => void
+  onError?: () => void
+}
+
 type FilterParams<T> = {
   todos: T[]
   search: string
@@ -13,11 +19,26 @@ const get = async ({ page }: GetParams) => {
   return todosRepository.get({ page: page, limit: 2 })
 }
 
+const post = async <T>({ content, onSuccess, onError }: PostParams<T>) => {
+  if (!content) {
+    return onError?.()
+  }
+
+  const todo = {
+    id: crypto.randomUUID(),
+    date: new Date().toISOString(),
+    content,
+    done: false,
+  }
+  onSuccess?.(todo as T)
+}
+
 const filterByContent = <T extends { content: string }>({ todos, search }: FilterParams<T>): T[] => {
   return todos.filter((todo) => todo.content.toLowerCase().includes(search.toLowerCase()))
 }
 
 export const todosController = {
   get,
+  post,
   filterByContent,
 }
