2ffd6049939fe715d040d50f216f730e7e0cb778
Adeonir Kohl <adeonir@gmail.com>
2023-08-16 21:34:12 -0300

    add post function to home page

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -4,8 +4,11 @@ import { todosController } from '~/backend/controllers/todos'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    todosController.get(req, res)
-    return
+    return todosController.get(req, res)
+  }
+
+  if (req.method === 'POST') {
+    return todosController.post(req, res)
   }
 
   res.status(405).json({ message: 'Method not allowed' })
