5e8ac28e3e39a6cb8a4896f946449706264cf630
Adeonir Kohl <adeonir@gmail.com>
2023-08-18 19:02:18 -0300

    add delete by id to backend controller

--- src/backend/controllers/todos.ts
+++ src/backend/controllers/todos.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { todosRepository } from '~/backend/repository/todos'
 import { z } from 'zod'
+import { HttpNotFoundError } from '../infra/errors'
 
 const findAll = async (req: NextApiRequest, res: NextApiResponse) => {
   const query = req.query
@@ -64,8 +65,31 @@ const toggleDone = async (req: NextApiRequest, res: NextApiResponse) => {
     })
 }
 
+const deleteById = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query
+  const parsed = z.string().uuid().nonempty().safeParse(id)
+
+  if (!parsed.success) {
+    return res.status(400).json({ error: { message: `This id ${id} is not valid` } })
+  }
+
+  await todosRepository
+    .deleteById({ id: parsed.data })
+    .then(() => {
+      return res.status(204).end()
+    })
+    .catch((error) => {
+      if (error instanceof HttpNotFoundError) {
+        return res.status(error.status).json({ error: { message: `Todo with id ${id} not found` } })
+      }
+
+      return res.status(500).json({ error: { message: 'Internal server error' } })
+    })
+}
+
 export const todosController = {
   findAll,
   createNew,
   toggleDone,
+  deleteById,
 }
