935feb21fcd604664ac638209f991493906cf462
Adeonir Kohl <adeonir@gmail.com>
2023-08-21 09:58:08 -0300

    update cypress config increasing the timeout time

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents() {},
   },
   video: false,
   screenshotOnRunFailure: false,
+  defaultCommandTimeout: 10000,
 })
