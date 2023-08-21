a8378aef25ee5bee3d9b60c8333d679191530ec7
Adeonir Kohl <adeonir@gmail.com>
2023-08-21 18:56:44 -0300

    refactor project to use next app router

---
+++ registry.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import type { ReactNode } from 'react'
+import React, { useState } from 'react'
+import { useServerInsertedHTML } from 'next/navigation'
+import { StyleRegistry, createStyleRegistry } from 'styled-jsx'
+
+export default function StyledJsxRegistry({ children }: { children: ReactNode }) {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const [jsxStyleRegistry] = useState(() => createStyleRegistry())
+
+  useServerInsertedHTML(() => {
+    const styles = jsxStyleRegistry.styles()
+    jsxStyleRegistry.flush()
+    return <>{styles}</>
+  })
+
+  return <StyleRegistry registry={jsxStyleRegistry}>{children}</StyleRegistry>
+}
